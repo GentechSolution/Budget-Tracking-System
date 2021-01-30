59a5cd54e75b396e38c32fbb51ed79e7f431f69e
Mbuso Ngobeni <MBUSO16@discovery.co.za>
2021-01-30 14:14:21 +0200

    Add project files.

---
+++ RegistrationAndLogin/Controllers/FormController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //displays the accomodation expense form
+        public ActionResult AccomodationExpenses() 
+        {
+            return View();
+        }
+        //displays the monthly expenses form
+        public ActionResult MonthlyExpenses() 
+        {
+            return View();
+        }
+        //displays the vehicle expenses form
+        public ActionResult VehicleExpenses() 
+        {
+            return View();
+        }
+    }
+}
---
+++ RegistrationAndLogin/Models/MonthlyExpenses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationAndLogin.Models
+{
+    public class MonthlyExpenses
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Gross Income is required")]
+        public string Gross_Income { get; set; }
+
+        public string Monthly_Tax { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Utilities is required")]
+        public string Utilities { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Travel Cost is required")]
+        public string Travel_Cost { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Phone Bill is required")]
+        public string Phone_Bill { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Other Expenses is required")]
+        public string Other_Expenses { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Net Income is required")]
+        public string Net_Income { get; set; }
+    }
+}
